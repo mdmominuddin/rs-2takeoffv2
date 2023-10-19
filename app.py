3979f61c051e9fe1a185941f28452b29828b9bf1
Replit user <>
2023-10-19 03:56:20 +0000

    addess templates

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
